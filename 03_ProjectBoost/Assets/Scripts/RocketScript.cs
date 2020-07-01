cc8e385f3e55f9a0fb22d9af6f00cb8bd4ce284a
Erik Post <erikpost@hotmail.com>
2020-07-01 23:40:33 +0200

    End of day commit

---
+++ 03_ProjectBoost/Assets/Scripts/RocketScript.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput() {
+        if (Input.GetKey(KeyCode.Space)) {
+            Debug.Log("Space pressed");
+        }
+
+        if(Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D)) {
+            Debug.Log("Left and/or Right pressed");
+        }else if (Input.GetKey(KeyCode.A)) {
+            Debug.Log("Left pressed");
+        }else if (Input.GetKey(KeyCode.D)) {
+            Debug.Log("Right pressed");
+        }
+    }
+}
