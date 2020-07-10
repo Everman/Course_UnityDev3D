0100d00de54a5335f052793c58d89063921c070f
Erik Post <erikpost@hotmail.com>
2020-07-10 17:33:00 +0200

    Added Splash screen
    Added space ship

---
+++ 04_ArgonAssault/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadNextScene", levelLoadDelay);
+    }
+
+    void LoadNextScene() {
+        SceneManager.LoadScene(1);
+    }
+}
