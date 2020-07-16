57fc0fc8430eeeb436016b985347b0c30f719316
Erik Post <erikpost@hotmail.com>
2020-07-16 14:18:22 +0200

    Lesson 4.97

--- 04_ArgonAssault/Assets/Scripts/LevelLoader.cs
+++ 04_ArgonAssault/Assets/Scripts/LevelLoader.cs
@@ -1,15 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
     [SerializeField] float levelLoadDelay = 2;
-
-    private void Awake() {
-        DontDestroyOnLoad(gameObject);
-    }
 
     // Start is called before the first frame update
     void Start()
