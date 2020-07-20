739d0fc3eaf00e9502f3c97f88d1482e5c4e1ab8
Erik Post <erikpost@hotmail.com>
2020-07-21 00:02:22 +0200

    Lesson 4.109 part 1

--- 04_ArgonAssault/Assets/Scripts/Enemy.cs
+++ 04_ArgonAssault/Assets/Scripts/Enemy.cs
@@ -8,14 +8,22 @@ public class Enemy : MonoBehaviour
     [Tooltip("Time it takes before enemy ship is removed from game")] [SerializeField] float deathDelay = 1f;
     [Tooltip("Parent where DeathFX should be bound to")][SerializeField] GameObject fxParent = null;
 
+    private ScoreBoard scoreBoard;
+
     private bool alive = true;
 
     private void Start() {
-        // Null checks
-        if(deathFX == null) { Debug.LogError("ERROR - Enemy deathFX does not hold a GameObject for: " + this.name); }
-        if(fxParent == null) { Debug.LogError("ERROR - Enemy fxParent does not hold a GameObject for: " + this.name); }
-        
         AddNonTriggerBoxCollider();
+        
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+        
+        NullChecks();
+    }
+
+    private void NullChecks() {
+        if (deathFX == null) { Debug.LogError("ERROR - Enemy deathFX does not hold a GameObject for: " + this.name); }
+        if (fxParent == null) { Debug.LogError("ERROR - Enemy fxParent does not hold a GameObject for: " + this.name); }
+        if (scoreBoard == null) { Debug.LogError("Error - Enemy script could not find a scoreboard object!"); }
     }
 
     private void AddNonTriggerBoxCollider() {
@@ -27,6 +35,7 @@ public class Enemy : MonoBehaviour
     {
         if (alive) {
             alive = false;
+            scoreBoard.ScoreHit();
             GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
             fx.transform.parent = fxParent.transform;
             Invoke("DestroyEnemy", deathDelay);
