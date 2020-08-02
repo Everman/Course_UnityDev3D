37e87957954d3b48f16d9ea36c41d97df992449a
Erik Post <erikpost@hotmail.com>
2020-08-02 13:14:26 +0200

    Lesson 5.142 Added propper meshes and towers looking at enemies

---
+++ RealmRush/Assets/Scripts/Tower.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] Transform objectToPan;
+    [SerializeField] Transform targetEnemy;
+
+    private void Start() {
+        if (objectToPan == null) { Debug.LogError("objectToPan = null"); }
+        if (targetEnemy == null) { Debug.LogError("targetEnemy = null"); }
+    }
+
+    private void LookAt() {
+        objectToPan.LookAt(targetEnemy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookAt();
+    }
+}
