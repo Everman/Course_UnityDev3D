71ad4626e6700afc40d4fb61699f7ac3f9e2a0cb
Erik Post <erikpost@hotmail.com>
2020-08-11 23:25:24 +0200

    Added Post Processing Package

--- RealmRush/Assets/Scripts/EnemySpawner.cs
+++ RealmRush/Assets/Scripts/EnemySpawner.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour
 {
@@ -9,20 +10,31 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] Enemy enemyToSpawn = null;
     [SerializeField] GameObject enemyParent = null;
     [SerializeField] GameObject particleParent = null;
+    [SerializeField] Text numberOfEnemiesTest = null;
+    int numberOfEnemies = 0;
 
     private void Start() {
+        numberOfEnemiesTest.text = numberOfEnemies.ToString();
         StartCoroutine(SpawnEnemies());
     }
 
     IEnumerator SpawnEnemies() {
         while (spawnEnemies) {
             Enemy spawnedEnemy = Instantiate(enemyToSpawn, transform.position, Quaternion.identity);
-            if(enemyParent != null) {
+
+            AddScore();
+
+            if (enemyParent != null) {
                 spawnedEnemy.transform.parent = enemyParent.transform;
                 spawnedEnemy.SetParticleParent(particleParent);
             }
-            
+
             yield return new WaitForSeconds(secondsBetweenSpawns);
         }
+    }
+
+    private void AddScore() {
+        numberOfEnemies++;
+        numberOfEnemiesTest.text = numberOfEnemies.ToString();
     }
 }
--- RealmRush/Assets/Scripts/PlayerHealth.cs
+++ RealmRush/Assets/Scripts/PlayerHealth.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] int playerHealth = 5;
+    [SerializeField] Text healthText = null;
+
+    private void Start() {
+        healthText.text = playerHealth.ToString();
+    }
 
     private void OnTriggerEnter(Collider other) {
         playerHealth--;
-        if(playerHealth < 1) {
+        healthText.text = playerHealth.ToString();
+        if (playerHealth < 1) {
             Debug.Log("Death");
         }
     }
