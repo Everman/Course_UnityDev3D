b95b0d744dbf133d78da130852a4ceebda9e57f9
Erik Post <erikpost@hotmail.com>
2020-08-11 18:30:25 +0200

    Lesson 5.159 Added base health and damage

---
+++ RealmRush/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] int playerHealth = 5;
+
+    private void OnTriggerEnter(Collider other) {
+        playerHealth--;
+        if(playerHealth < 1) {
+            Debug.Log("Death");
+        }
+    }
+}
