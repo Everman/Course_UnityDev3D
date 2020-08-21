eba43b04d0f21d9be50b68be73737183c7940091
Erik Post <erikpost@hotmail.com>
2020-08-21 21:58:56 +0200

    Lesson 6.170 : Added Enemies with NavMeshAgent

---
+++ 06_FPS/Assets/Scripts/EnemyMovementAI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovementAI : MonoBehaviour
+{
+
+    [SerializeField] Transform target = null;
+    NavMeshAgent navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+}
