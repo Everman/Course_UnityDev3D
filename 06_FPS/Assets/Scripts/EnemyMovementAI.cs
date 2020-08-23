141e9ef76da7e021cdb9a55dfe2665cc4c07c226
Erik Post <erikpost@hotmail.com>
2020-08-23 11:50:22 +0200

    Lesson 6.174

--- 06_FPS/Assets/Scripts/EnemyMovementAI.cs
+++ 06_FPS/Assets/Scripts/EnemyMovementAI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -13,6 +14,7 @@ public class EnemyMovementAI : MonoBehaviour
     
     NavMeshAgent navMeshAgent;
     float distanceToTarget = Mathf.Infinity;
+    bool isProvoked;
 
     void Start()
     {
@@ -20,23 +22,42 @@ public class EnemyMovementAI : MonoBehaviour
     }
 
     void Update() {
-        navMeshAgent.SetDestination(target.position);
-        ChaseCheck();
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        
+        if ( isProvoked ) {
+            EngageTarget();
+        } else if ( distanceToTarget <= chaseRange ) {
+            isProvoked = true;            
+        }
     }
 
-    private void ChaseCheck() {
-        distanceToTarget = Vector3.Distance(target.position, transform.position);
-        if (distanceToTarget <= chaseRange) {
-            navMeshAgent.isStopped = false;
-        } else {
-            navMeshAgent.isStopped = true;
+    private void EngageTarget() {
+        if ( distanceToTarget >= navMeshAgent.stoppingDistance ) {
+            ChaseTarget();
         }
+        
+        if (distanceToTarget < navMeshAgent.stoppingDistance) { 
+            AttackTarget();
+        }
+    }
+
+    private void ChaseTarget() {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void AttackTarget() {
+        Debug.Log("Attack my boi");
     }
 
     void OnDrawGizmos() {
         if (debug) {
-            Gizmos.color = new Color(1, 1, 0, 0.75F);
+            Gizmos.color = Color.yellow;
             Gizmos.DrawWireSphere(transform.position, chaseRange);
+
+            if (navMeshAgent) {
+                Gizmos.color = Color.red;
+                Gizmos.DrawWireSphere(transform.position, navMeshAgent.stoppingDistance);
+            }
         }
     }
 }
